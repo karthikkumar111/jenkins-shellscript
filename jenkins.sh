d065765a7309c45d7e953188d8ad4446479cd5b4
karthikkumar111 <131590180+karthikkumar111@users.noreply.github.com>
2025-05-25 18:44:15 +0530

    Create jenkins.sh

---
+++ jenkins.sh
@@ -0,0 +1,10 @@
+yum install git -y
+#java dependency for jenkins
+sudo yum install java-17-amazon-corretto -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum install jenkins -y
+systemctl restart jenkins
+systemctl status jenkins
+cat /var/lib/jenkins/secrets/initialAdminPassword
