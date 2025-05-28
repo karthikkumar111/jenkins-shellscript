c2bb683b12dd54c89acbc190ee9fdf61da5f1b6e
karthikkumar111 <131590180+karthikkumar111@users.noreply.github.com>
2025-05-28 18:11:22 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -3,7 +3,7 @@ yum install git -y
 
 
 #maven setup
-sudo yum install -y java-17-amazon-corretto-devel
+sudo yum install java-17-amazon-corretto -y
 wget https://dlcdn.apache.org/maven/maven-3/3.9.6/binaries/apache-maven-3.9.6-bin.tar.gz
 tar -zxvf *tar.gz
 yum install maven -y
