81bbc93d188b4cec262df67c5e52b0ba13ecd3d2
karthikkumar111 <131590180+karthikkumar111@users.noreply.github.com>
2025-05-29 15:48:24 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -2,6 +2,7 @@
 yum install git -y
 
 #java dependency for jenkins(sudo yum install java-17-amazon-corretto -y)
+sudo yum install java-17-amazon-corretto -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
