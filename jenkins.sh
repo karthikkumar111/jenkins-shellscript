c9b7973942c205cf23cc9a8b7c77c2423415e5f9
karthikkumar111 <131590180+karthikkumar111@users.noreply.github.com>
2025-05-29 08:15:09 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,14 +1,6 @@
 #git setup
 yum install git -y
 
-
-#maven setup
-sudo yum install java-17-amazon-corretto -y
-wget https://dlcdn.apache.org/maven/maven-3/3.9.6/binaries/apache-maven-3.9.6-bin.tar.gz
-tar -zxvf *tar.gz
-yum install maven -y
-
-
 #java dependency for jenkins(sudo yum install java-17-amazon-corretto -y)
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
