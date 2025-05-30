bb7b4fe1628c09ae5d44109e20f1470314359fbb
karthikkumar111 <131590180+karthikkumar111@users.noreply.github.com>
2025-05-30 08:21:49 +0530

    Update jenkins.sh
    git, maven, java 1.8.0, jenkins, java 17

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,14 @@
 #git setup
 yum install git -y
 
+
+#maven setup
+sudo yum install java-1.8.0-openjdk -y
+wget https://dlcdn.apache.org/maven/maven-3/3.9.9/binaries/apache-maven-3.9.9-bin.tar.gz
+tar -zxvf *tar.gz
+yum install maven -y
+
+
 #jenkins setup
 #java dependency for jenkins(sudo yum install java-17-amazon-corretto -y)
 sudo yum install java-17-amazon-corretto -y
