dc920f2a9f1faa5cd0bc5c8661af8c5c756914ca
karthikkumar111 <131590180+karthikkumar111@users.noreply.github.com>
2025-08-03 07:53:20 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -4,7 +4,7 @@ yum install git -y
 
 #maven setup
 sudo yum install java-1.8.0-openjdk -y
-wget https://dlcdn.apache.org/maven/maven-3/3.9.9/binaries/apache-maven-3.9.9-bin.tar.gz
+wget https://dlcdn.apache.org/maven/maven-3/3.9.11/binaries/apache-maven-3.9.11-bin.tar.gz
 tar -zxvf *tar.gz
 yum install maven -y
 
